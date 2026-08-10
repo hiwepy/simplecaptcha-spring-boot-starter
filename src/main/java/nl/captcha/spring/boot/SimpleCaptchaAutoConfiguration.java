6996ae6ede43dda7d03bc2b953760bcfe602bf81
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 15:58:58 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/nl/captcha/spring/boot/SimpleCaptchaAutoConfiguration.java
+++ src/main/java/nl/captcha/spring/boot/SimpleCaptchaAutoConfiguration.java
@@ -18,7 +18,7 @@ import nl.captcha.servlet.StickyCaptchaServlet;
 @Configuration
 @ConditionalOnClass({ Captcha.class })
 @EnableConfigurationProperties(SimpleCaptchaProperties.class)
-/**\n * Auto-configuration for SimpleCaptchaAutoConfiguration.\n *\n * @author [@Loong Wan](https://github.com/loong10k)\n * @since 1.0.0\n */
+/**\n * Auto-configuration for SimpleCaptchaAutoConfiguration.\n *\n * @author <a href="https://github.com/loong10k">Loong Wan</a>\n * @since 1.0.0\n */
 public class SimpleCaptchaAutoConfiguration {
 
     private static final String PARAM_HEIGHT = "height";
